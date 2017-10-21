d214375fba214f94426fcfe4671b26a43f0efd62
Nirgalz <znirgal@gmail.com>
2017-10-21 18:39:42 +0200

    minions will starve to death.

---
+++ js/Minion.js
@@ -0,0 +1,38 @@
+class Minion{
+
+    constructor(id){
+        this.id = id;
+        this.health = 50;
+        this.hunger = 50;
+        this.thirst = 10;
+        this.inventory = {};
+        this.birthday = new Date().getTime();
+        this.starve();
+    }
+
+    getAge() {
+        let nowTime = new Date().getTime();
+        let t = nowTime - this.birthday;
+
+        let h = ('0' + Math.floor(t / 3600000)).slice(-2);
+        let m = ('0' + Math.floor(t % 3600000 / 60000)).slice(-2);
+        let s = ('0' + Math.floor(t % 60000 / 1000)).slice(-2);
+        return  h + ':' + m + ':' + s;
+    }
+
+    starve() {
+        let minion = this;
+        setInterval(function () {
+            if (minion.health <= 0) {
+                console.log(minion.id + " has died from starving")
+            } else {
+                minion.health = minion.health - (minion.hunger /100);
+            }
+        }, 10000)
+    }
+
+
+
+
+
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+"use strict";
+
 $(function () {
 
     let map = new MapGen(12,20);
+    let population = populate(0);
+
+    console.log(population);
+
+    $('#test').on('click', function () {
+        console.log(population[0]);
+    });
 
     function mapViz(map) {
         for (let i = 0 ; i <= map.x ; i++) {
@@ -18,7 +27,14 @@ $(function () {
     mapViz(map);
 
 
-    console.log(map);
+    function populate(num) {
+        let pop = [];
+        for (let i = 0; i <= num ; i++) {
+            pop[i] = new Minion(i);
+        }
+        return pop;
+    }
+
 
 });
 
