1cb49cdf9336d89f525daa798a31f8f683cfff1d
Nirgalz <znirgal@gmail.com>
2017-10-21 12:43:21 +0200

    tiles resources that show on hover

--- js/MapGen.js
+++ js/MapGen.js
@@ -15,6 +15,7 @@ class MapGen {
                 landscape[index].x = i;
                 landscape[index].y = j;
                 landscape[index].type = this.randomTiles();
+                landscape[index].resources = this.resourcesGen(landscape[index].type);
                 index++;
             }
         }
@@ -24,7 +25,7 @@ class MapGen {
     randomTiles() {
         let tileType = '';
 
-        let randomInt = Math.floor((Math.random() * 100) + 1);
+        let randomInt = this.randomInt();
         if (randomInt <= 25) {
             tileType = 'water';
         } else if (randomInt <= 50) {
@@ -35,6 +36,34 @@ class MapGen {
             tileType = 'forest';
         }
         return tileType;
+    }
+
+    randomInt(){
+        return Math.floor((Math.random() * 100) + 1);
+    }
+
+    resourcesGen(type){
+        let resources = {};
+        switch (type){
+            case 'water':
+                resources.fish = this.randomInt();
+                resources.water = this.randomInt();
+                break;
+            case 'dirt':
+                resources.worm = this.randomInt();
+                resources.dirt = this.randomInt();
+                break;
+            case 'grass':
+                resources.grass = this.randomInt();
+                resources.sheep = this.randomInt();
+                break;
+            case 'forest':
+                resources.wood = this.randomInt();
+                resources.boar = this.randomInt();
+                resources.berry = this.randomInt();
+                break;
+        }
+        return resources;
     }
 
 }
--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(function () {
 
-    let map = new MapGen(5,8);
+    let map = new MapGen(12,20);
 
     function mapViz(map) {
         for (let i = 0 ; i <= map.x ; i++) {
             $('#map').append('<tr id="row-'+ i+'"></tr>');
             for (let j = 0 ; j <= map.y ; j++) {
-                $('#row-'+i).append('<td id=tile-'+ i + j +'></td>')
+                $('#row-'+i).append('<td id=tile-'+ i +'-'+ j +' title=>')
             }
         }
 
         map.landscape.forEach(function (elem) {
-            $('#tile-'+elem.x + elem.y).addClass(elem.type);
+            $('#tile-'+elem.x +'-'+ elem.y).addClass(elem.type).attr('title', 'res'+JSON.stringify(elem.resources));
         })
     }
 
