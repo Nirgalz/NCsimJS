6e325917241c8092ba9a20fe7aa130d501f3f022
nirgal <znirgal@gmail.com>
2017-11-03 11:54:14 +0000

    updates wip

--- js/script.js
+++ js/script.js
@@ -1,39 +1,53 @@
 "use strict";
 
 $(function () {
-
-    //map and seed pop parameters
-    let mapSize = [4,7];
-    let seedPopParam = 3;
-    let simSpeedParam = 10;
     
-    let Display = new DisplayManager();
-
-    //generates map and pop
-    let map = new MapGen(mapSize);
-    let population = populate(seedPopParam, map);
-
-
-    let AImgr = new AIManager(population,map, simSpeedParam);
-    AImgr.start();
-
-    $('#test').on('click', function ()
-    {
-        console.log(population);
-    });
+    let Display = {};
+    let map ={};
+    let population = {};
+    let AImgr = {};
+    let simSpeedParam = [];
     
+    function initSim(mapSize, seedPopParam, simSpeedParam){
+        
+ //map and seed pop parameters
+        Display = new DisplayManager();
+        //generates map and pop
+        map = new MapGen(mapSize);
+       population = populate(seedPopParam, map);
+
+
+        AImgr = new AIManager(population,map, simSpeedParam);
+        AImgr.start();
+    }
+    
+    initSim([4,7] , 3 , 10 );
+  
+    
+
+
+  
     $('#checkData').on('click', function ()
     {
         AImgr.checkData();
     });
 
     $('#spawnMinion').on('click', function () {
-        population.push(new Minion(population.length, randomIntInRange(map.x), randomIntInRange(map.y), [map.x,map.y], simSpeedParam))
+        population.push(new Minion(population.length, randomIntInRange(map.x), randomIntInRange(map.y), map, simSpeedParam))
     });
 
     $('#killMinion').on('click', function () {
         population.splice(-1,1);
     });
+    
+    $('#genBtn').on('click', function(e) {
+        e.preventDefault();
+        let x = $('#xmax').val();
+        let y = $('#ymax').val();
+        let seed = $('#seed').val();
+        
+        console.log(x);
+    })
 
     
 
