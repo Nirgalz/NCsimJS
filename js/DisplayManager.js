8c58fcbaf1ad0872c3472aa1638ab5e7646443da
Nirgalz <znirgal@gmail.com>
2017-11-05 11:59:14 +0100

    pixi tests, almost there

--- js/DisplayManager.js
+++ js/DisplayManager.js
@@ -1,40 +1,155 @@
-class DisplayManager
-{
+class DisplayManager {
+
+    constructor(map, pop) {
+        this.map = map;
+        this.pop = pop;
+        // this.stage = new PIXI.Container();
+        // this.renderer = PIXI.autoDetectRenderer(512, 512);
+        // this.loader = PIXI.loader;
+        // this.resources = PIXI.loader.resources;
+        // this.Sprite = PIXI.Sprite;
+        //this.pixiDisplay(this.map, this.pop);
+
+    }
+
+
+
+    pixiDisplay(map, pop) {
+
+        //pixi.js tests
+
+
+        //Create the renderer
+
+        let stage = new PIXI.Container();
+        let renderer = PIXI.autoDetectRenderer(512, 512);
+        let loader = PIXI.loader;
+        let resources = PIXI.loader.resources;
+        let Sprite = PIXI.Sprite;
+
+
+//Add the canvas to the HTML document
+        $('#pixiMap').append(renderer.view);
+
+//Create a container object called the `stage`
+
+        loader
+            .add("media/face-3-1.png")
+            .add("media/terrain/water.png")
+            .add("media/terrain/dirt.png")
+            .add("media/terrain/grass.png")
+            .add("media/terrain/forest.png")
+            .load(setupPixi);
+
+        function setupPixi() {
+
+            for (let i = 0; i < map.landscape.length; i++) {
+
+                let tile = new Sprite(resources["media/terrain/" + map.landscape[i].type + ".png"].texture);
+                tile.x = map.landscape[i].x * 100;
+                tile.y = map.landscape[i].y * 100;
+                stage.addChild(tile);
+            }
+
+            //pop sprites
+
+
+            for (let k = 0; k <= pop.length - 1; k++) {
+
+                if (pop[k].isAlive === true) {
+                    let face = new Sprite(
+                        resources["media/face-3-1.png"].texture
+                    );
+                    face.x = pop[k].xCoordinate * 100;
+                    face.y = pop[k].yCoordinate * 100;
+
+                    stage.addChild(face);
+
+                }
+                else if (pop[k].isAlive === false) {
+
+                }
+
+                //$('#minionInfo').append('<tr><td>'+pop[k].id+'</td><td>'+pop[k].status+'</td><td>'+Math.floor(pop[k].health)+'</td><td>'+Math.floor(pop[k].hunger)+'</td><td>'+pop[k].inventory.food+'</td><td>'+pop[k].fatigue+'</td>')
+            }
+
+//Tell the `renderer` to `render` the `stage`
+            renderer.render(stage);
+        }
+
+
+    }
+
+
+
+
+
+    pixiLoad() {
+        //terrain sprites
+        for (let i = 0; i < this.map.landscape.length; i++) {
+
+            let tile = new this.Sprite(this.resources["media/terrain/" + this.map.landscape[i].type + ".png"].texture);
+            tile.x = this.map.landscape[i].x * 100;
+            tile.y = this.map.landscape[i].y * 100;
+            this.stage.addChild(tile);
+        }
+
+        //pop sprites
+
+
+        for (let k = 0; k <= this.pop.length - 1; k++) {
+
+            if (this.pop[k].isAlive === true) {
+                let face = new this.Sprite(
+                    this.resources["media/face-3-1.png"].texture
+                );
+                face.x = this.pop[k].xCoordinate * 100;
+                face.y = this.pop[k].yCoordinate * 100;
+
+                this.stage.addChild(face);
+
+            }
+            else if (this.pop[k].isAlive === false) {
+
+            }
+
+            //$('#minionInfo').append('<tr><td>'+pop[k].id+'</td><td>'+pop[k].status+'</td><td>'+Math.floor(pop[k].health)+'</td><td>'+Math.floor(pop[k].hunger)+'</td><td>'+pop[k].inventory.food+'</td><td>'+pop[k].fatigue+'</td>')
+        }
+
+        return this.stage;
+    }
+
+
     //map visualisation
     mapViz(map) {
         $('#map').empty();
-        for (let i = 0 ; i <= map.x ; i++)
-        {
-            $('#map').append('<tr id="row-'+ i+'"></tr>');
-            for (let j = 0 ; j <= map.y ; j++) {
-                $('#row-'+i).append('<td onclick="showTile('+i+','+j+')" id=tile-'+ i +'-'+ j +' ></td>')
+        for (let i = 0; i <= map.x; i++) {
+            $('#map').append('<tr id="row-' + i + '"></tr>');
+            for (let j = 0; j <= map.y; j++) {
+                $('#row-' + i).append('<td onclick="showTile(' + i + ',' + j + ')" id=tile-' + i + '-' + j + ' ></td>')
             }
         }
 
-        map.landscape.forEach(function (elem)
-        {
-            $('#tile-'+elem.x +'-'+ elem.y).addClass(elem.type).append('<progress value="' + elem.resources.food + '" max="100"></progress>');
+        map.landscape.forEach(function (elem) {
+            $('#tile-' + elem.x + '-' + elem.y).addClass(elem.type).append('<progress value="' + elem.resources.food + '" max="100"></progress>');
         })
     }
-    
+
     minionViz(pop) {
         $('#minionInfo').empty();
-        for ( let k = 0 ; k<=pop.length - 1 ; k++)
-        {
+        for (let k = 0; k <= pop.length - 1; k++) {
             let coords = '#tile-' + pop[k].xCoordinate + '-' + pop[k].yCoordinate;
-            if (pop[k].isAlive === true)
-            {
-               
-               $(coords).append('<img src="media/face-3-1.png" id=minion-'+(k +1) +'>') ;
-               // $(coords).append('<i class="fa fa-user-circle" aria-hidden="true" id=minion-'+(k +1) +'></i>') ;
+            if (pop[k].isAlive === true) {
+
+                $(coords).append('<img src="media/face-3-1.png" id=minion-' + (k + 1) + '>');
+                // $(coords).append('<i class="fa fa-user-circle" aria-hidden="true" id=minion-'+(k +1) +'></i>') ;
             }
-            else if (pop[k].isAlive === false)
-            {
-                $(coords).append('<img src="media/face-3-4.png" id=minion-'+(k +1) +'>') ;
+            else if (pop[k].isAlive === false) {
+                $(coords).append('<img src="media/face-3-4.png" id=minion-' + (k + 1) + '>');
                 //$(coords).append('<i class="fa fa-plus-square" aria-hidden="true" id=minion-'+(k +1) +'></i>') ;
             }
 
-            $('#minionInfo').append('<tr><td>'+pop[k].id+'</td><td>'+pop[k].status+'</td><td>'+Math.floor(pop[k].health)+'</td><td>'+Math.floor(pop[k].hunger)+'</td><td>'+pop[k].inventory.food+'</td><td>'+pop[k].fatigue+'</td>')
+            $('#minionInfo').append('<tr><td>' + pop[k].id + '</td><td>' + pop[k].status + '</td><td>' + Math.floor(pop[k].health) + '</td><td>' + Math.floor(pop[k].hunger) + '</td><td>' + pop[k].inventory.food + '</td><td>' + pop[k].fatigue + '</td>')
         }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,37 +1,34 @@
 "use strict";
 
 $(function () {
-    
+
     let Display = {};
-    let map ={};
+    let map = {};
     let population = {};
     let AImgr = {};
     let TimeMgr = {};
     let simSpeedParam = [1];
-    
-    function initSim(mapSize, seedPopParam, simSpeedParam){
-        
- //map and seed pop parameters
+
+    function initSim(mapSize, seedPopParam, simSpeedParam) {
+
+        //map and seed pop parameters
         Display = new DisplayManager();
         //generates map and pop
         map = new MapGen(mapSize);
         population = populate(seedPopParam, map);
 
 
-        AImgr = new AIManager(population,map, simSpeedParam, TimeMgr);
+        AImgr = new AIManager(population, map, simSpeedParam, TimeMgr);
         AImgr.start();
-        
+
         TimeMgr = new TimeManager(simSpeedParam, map, population, AImgr);
         TimeMgr.play();
     }
-    
-    initSim([4,7] , 3 , 10 );
-  
-    
 
-  
-    $('#checkData').on('click', function ()
-    {
+    initSim([5, 5], 3, 10);
+
+
+    $('#checkData').on('click', function () {
         AImgr.checkData();
     });
 
@@ -40,87 +37,78 @@ $(function () {
     });
 
     $('#killMinion').on('click', function () {
-        population.splice(-1,1);
+        population.splice(-1, 1);
     });
-    
-    $('#genBtn').on('click', function(e) {
+
+    $('#genBtn').on('click', function (e) {
         e.preventDefault();
         let x = $('#xmax').val();
         let y = $('#ymax').val();
         let seed = $('#seed').val();
-        
-        initSim([x,y], seed, 10);
+
+        initSim([x, y], seed, 10);
     })
-    
-    
-    $('#speedMinus').on('click', function() {
-        TimeMgr.speedFactor +=1;
-        $('#speedometer').html(TimeMgr.speedCalc()) ;
+
+
+    $('#speedMinus').on('click', function () {
+        TimeMgr.speedFactor += 1;
+        $('#speedometer').html(TimeMgr.speedCalc());
     })
-    
-    $('#speedPlus').on('click', function() {
-        TimeMgr.speedFactor -=1;
-        $('#speedometer').html(TimeMgr.speedCalc()) ;
+
+    $('#speedPlus').on('click', function () {
+        TimeMgr.speedFactor -= 1;
+        $('#speedometer').html(TimeMgr.speedCalc());
     })
-    
-    $('#speedPause').on('click', function() {
+
+    $('#speedPause').on('click', function () {
         if (TimeMgr.pause === true) {
             TimeMgr.pause = false;
             TimeMgr.play();
-        } else if (TimeMgr.pause === false){
-             TimeMgr.pause = true;
+        } else if (TimeMgr.pause === false) {
+            TimeMgr.pause = true;
         }
-       
-    })
 
-    
+    })
 
 
     //generates the adequate number of minions in random coordinates
-    function populate(num)
-    {
+    function populate(num) {
 
         Display.mapViz(map);
         let pop = [];
-        for (let i = 0; i < num ; i++)
-        {
+        for (let i = 0; i < num; i++) {
             pop[i] = new Minion(i, randomIntInRange(map.x), randomIntInRange(map.y), map, 1);
         }
         Display.minionViz(pop);
-        
+
         return pop;
 
     }
-    
-    let refreshDisplay = setInterval(function(){
+
+    let pixiDis = new DisplayManager(map, population);
+    pixiDis.pixiDisplay(map,population);
+
+
+
+    let refreshDisplay = setInterval(function () {
         Display.mapViz(map);
         Display.minionViz(population);
 
-    },100);
-    
-    
-    function showTile(x,y){
-        
+    }, 100);
+
+
+    function showTile(x, y) {
+
         $('#tileInfo').text()
     }
-    
-    
+
 
     //randomizes from 1 to maxRange
     function randomIntInRange(maxRange) {
-            return Math.floor((Math.random() * maxRange)+1);
+        return Math.floor((Math.random() * maxRange) + 1);
     }
 
 
-
-
-    //pixi.js tests
-    
-    function setup () {
-        gameScene = new Container();
-        stage.addChild(gameScene);
-    }
-    
 
 
 });
