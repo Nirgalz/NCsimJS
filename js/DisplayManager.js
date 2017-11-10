c47c9330466c546af58fc25ba0098d4d6e8202c8
Nirgalz <znirgal@gmail.com>
2017-11-10 19:38:21 +0100

    working on hover tiles

--- js/DisplayManager.js
+++ js/DisplayManager.js
@@ -30,6 +30,8 @@ class DisplayManager {
         let ticker = PIXI.ticker.shared;
         ticker.autoStart = false;
 
+        let firstTick = true;
+
         ticker.stop();
 
 
@@ -52,47 +54,92 @@ class DisplayManager {
 
         function setupPixi() {
 
-            
 
-            //every in there will be updated
-            ticker.add(function (time) {
-                
-                //resets the scene each frame
-                stage.removeChildren(0, stage.children.length);
+            let style = {
+                font: '15px Courier, monospace',
+                fill: '#ffffff'
+            };
 
-
-            //landscape
             for (let i = 0; i < map.landscape.length; i++) {
 
+                //tiles
                 let tile = new Sprite(resources["media/terrain/" + map.landscape[i].type + ".png"].texture);
                 tile.x = map.landscape[i].x * 100;
                 tile.y = map.landscape[i].y * 100;
+
+
+                //makes tiles interactive and alpha- on hover
+                tile.interactive = true;
+
+
+                //tile.hitArea = new PIXI.Rectangle(map.landscape[i].x * 100, map.landscape[i].y * 100, 100, 100);
+                // make circle non-transparent when mouse is over it
+
+                tile.on('mouseover', onOver);
+                tile.on('mouseout', onOut);
+
+                function onOver(eventData) {
+                    this.alpha = 0.5;
+                }
+
+                function onOut(eventData) {
+                    this.alpha = 1;
+                }
+
                 stage.addChild(tile);
 
-                if (map.landscape[i].localBuilding === "campFire"){
-                    let building =  new Sprite(resources["media/buildings/campFire.png"].texture);
-                    building.x = (map.landscape[i].x * 100) + 20;
-                    building.y = (map.landscape[i].y * 100) + 20;
-                    stage.addChild(building)
-                }
-                
             }
+
+
+
+            //every in there will be updated
+            ticker.add(function (time) {
+
+                //resets the scene each frame
+
+                if (firstTick === false){
+                    stage.removeChildren(map.landscape.length, stage.children.length);
+
+                }
+                firstTick = false;
+
+                //food bars on tiles and wood
+
+
+                //landscape and data
+
+                let style = {
+                    font: '15px Courier, monospace',
+                    fill: '#ffffff'
+                };
+
+                for (let i = 0; i < map.landscape.length; i++) {
+
+
+
+                    //data
+                    let tileInfo = new PIXI.Text('food: ' + Math.floor(map.landscape[i].resources.food) + '\nwood: ' + Math.floor(map.landscape[i].resources.wood), style);
+                    tileInfo.x = map.landscape[i].x * 100;
+                    tileInfo.y = map.landscape[i].y * 100;
+
+                    stage.addChild(tileInfo);
+
+
+                    if (map.landscape[i].localBuilding === "campFire") {
+                        let building = new Sprite(resources["media/buildings/campFire.png"].texture);
+                        building.x = (map.landscape[i].x * 100) + 20;
+                        building.y = (map.landscape[i].y * 100) + 20;
+                        stage.addChild(building)
+                    }
+
+                }
+
+
 
                 //deletes minions from stage children so that this
                 // if (map.landscape.length !== stage.children.length) {
                 //     stage.removeChildren(map.landscape.length, stage.children.length);
                 // }
-
-                //food bars on tiles
-                for (let i = 0; i < map.landscape.length; i++) {
-
-                    let foodBar = new Sprite(resources["media/terrain/progress-food.png"].texture);
-                    foodBar.x = map.landscape[i].x * 100;
-                    foodBar.y = map.landscape[i].y * 100;
-                    foodBar.width = map.landscape[i].resources.food;
-
-                    stage.addChild(foodBar);
-                }
 
 
                 //pop sprites
@@ -124,11 +171,18 @@ class DisplayManager {
 
                     stage.addChild(face);
 
-
-                    $('#minionInfo').append('<tr><td>' + pop[k].id + '</td><td>' + pop[k].statusM + '</td><td>' + Math.floor(pop[k].health) + '</td><td>' + Math.floor(pop[k].hunger) + '</td><td>food: ' + pop[k].inventory.food + ' wood: ' + pop[k].inventory.wood +'</td><td>' + pop[k].fatigue + '</td>')
+                    // minion infos
+                    $('#minionInfo').append('<tr><td>' + pop[k].id + '</td><td>' + pop[k].statusM + '</td><td>' + Math.floor(pop[k].health) + '</td><td>' + Math.floor(pop[k].hunger) + '</td><td>food: ' + pop[k].inventory.food + ' wood: ' + pop[k].inventory.wood + '</td><td>' + pop[k].fatigue + '</td>')
                 }
 
-                renderer.render(stage);
+                // start animating
+                animate();
+
+                function animate() {
+                    requestAnimationFrame(animate);
+                    // render the root container
+                    renderer.render(stage);
+                }
             });
 
 
@@ -139,73 +193,4 @@ class DisplayManager {
     }
 
 
-    //for if i manage to get the setup function into an object somehow...
-    pixiLoad() {
-        //terrain sprites
-        for (let i = 0; i < this.map.landscape.length; i++) {
-
-            let tile = new this.Sprite(this.resources["media/terrain/" + this.map.landscape[i].type + ".png"].texture);
-            tile.x = this.map.landscape[i].x * 100;
-            tile.y = this.map.landscape[i].y * 100;
-            this.stage.addChild(tile);
-        }
-
-        //pop sprites
-
-
-        for (let k = 0; k <= this.pop.length - 1; k++) {
-
-            if (this.pop[k].isAlive === true) {
-                let face = new this.Sprite(
-                    this.resources["media/face-3-1.png"].texture
-                );
-                face.x = this.pop[k].xCoordinate * 100;
-                face.y = this.pop[k].yCoordinate * 100;
-
-                this.stage.addChild(face);
-
-            }
-            else if (this.pop[k].isAlive === false) {
-
-            }
-
-            //$('#minionInfo').append('<tr><td>'+pop[k].id+'</td><td>'+pop[k].status+'</td><td>'+Math.floor(pop[k].health)+'</td><td>'+Math.floor(pop[k].hunger)+'</td><td>'+pop[k].inventory.food+'</td><td>'+pop[k].fatigue+'</td>')
-        }
-
-        return this.stage;
-    }
-
-
-    //map visualisation
-    mapViz(map) {
-        $('#map').empty();
-        for (let i = 0; i <= map.x; i++) {
-            $('#map').append('<tr id="row-' + i + '"></tr>');
-            for (let j = 0; j <= map.y; j++) {
-                $('#row-' + i).append('<td onclick="showTile(' + i + ',' + j + ')" id=tile-' + i + '-' + j + ' ></td>')
-            }
-        }
-
-        map.landscape.forEach(function (elem) {
-            $('#tile-' + elem.x + '-' + elem.y).addClass(elem.type).append('<progress value="' + elem.resources.food + '" max="100"></progress>');
-        })
-    }
-
-    minionViz(pop) {
-        $('#minionInfo').empty();
-        for (let k = 0; k <= pop.length - 1; k++) {
-            let coords = '#tile-' + pop[k].xCoordinate + '-' + pop[k].yCoordinate;
-            if (pop[k].isAlive === true) {
-
-                $(coords).append('<img src="media/face-3-1.png" id=minion-' + (k + 1) + '>');
-                // $(coords).append('<i class="fa fa-user-circle" aria-hidden="true" id=minion-'+(k +1) +'></i>') ;
-            }
-            else if (pop[k].isAlive === false) {
-                $(coords).append('<img src="media/face-3-4.png" id=minion-' + (k + 1) + '>');
-                //$(coords).append('<i class="fa fa-plus-square" aria-hidden="true" id=minion-'+(k +1) +'></i>') ;
-            }
-
-            $('#minionInfo').append('<tr><td>' + pop[k].id + '</td><td>' + pop[k].status + '</td><td>' + Math.floor(pop[k].health) + '</td><td>' + Math.floor(pop[k].hunger) + '</td><td>' + pop[k].inventory.food + '</td><td>' + pop[k].fatigue + '</td>')
-        }
-    }
 }
