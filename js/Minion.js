8047fa593fb5dd0c9ade07e124eb88d806831221
nirgal <znirgal@gmail.com>
2017-11-15 10:18:40 +0000

    v

--- js/Minion.js
+++ js/Minion.js
@@ -185,9 +185,22 @@ class Minion {
                     break;
             }
         };
-
+        
+        //basic pathfinder
         let getToDestination = function (direction, it) {
-
+            let road = [];
+            let compass = {
+                N: {x:0,y:-1},
+                S: {x:0,y:1},
+                E: {x:1,y:0},
+                W: {x:-1,y:0},
+                NE: {x:1,y:-1},
+                NW: {x:-1,y:-1},
+                SE: {x:1,y:1},
+                SW: {x:-1,y:1}
+            } 
+            
+            
         };
 
 
